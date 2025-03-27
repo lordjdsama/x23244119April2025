4942905dfd8e0179f5210e75af53a6efe6c76b72
EC2 Default User <ec2-user@ip-172-31-13-10.eu-west-1.compute.internal>
2025-03-27 13:16:37 +0000

    static file setting

--- devopsPro/devopsPro/settings.py
+++ devopsPro/devopsPro/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Used by collectstatic command
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-LOGIN_REDIRECT_URL = '/profile/'
+LOGIN_REDIRECT_URL = '/home/'
