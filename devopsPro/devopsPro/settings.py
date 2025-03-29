8b29020ebf92d9d2fea1cf20b5363ab71624bdc9
EC2 Default User <ec2-user@ip-172-31-13-10.eu-west-1.compute.internal>
2025-03-29 11:23:08 +0000

    Include static files

--- devopsPro/devopsPro/settings.py
+++ devopsPro/devopsPro/settings.py
@@ -130,10 +130,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Used by collectstatic command
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+# Media files (user uploaded)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_REDIRECT_URL = 'address'
