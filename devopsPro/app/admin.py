39a185c2f23ceb220a8c7d4adb2970b6ea5c76f6
EC2 Default User <ec2-user@ip-172-31-13-10.eu-west-1.compute.internal>
2025-04-06 11:18:47 +0000

    pylint admin.py

--- devopsPro/app/admin.py
+++ devopsPro/app/admin.py
@@ -1,19 +1,20 @@
-from django.contrib import admin
-from . models import Product, Customer, Cart
+"""Admin configuration for Product, Customer, and Cart models."""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Product, Customer, Cart
 
 @admin.register(Product)
-class ProductionModelAdmin(admin.ModelAdmin):
+class ProductModelAdmin(admin.ModelAdmin):
+    """Admin configuration for the Product model."""
     list_display = ['id', 'title', 'discounted_price', 'category', 'product_image']
 
 @admin.register(Customer)
 class CustomerModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'name', 'mobile', 'locality', 'city', 'zipcode', 'state'] 
+    """Admin configuration for the Customer model."""
+    list_display = ['id', 'user', 'name', 'mobile', 'locality', 'city', 'zipcode', 'state']
     list_filter = ['state', 'city']
 
 @admin.register(Cart)
-class CartrModelAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'product', 'quantity'] 
-    
-    
+class CartModelAdmin(admin.ModelAdmin):
+    """Admin configuration for the Cart model."""
+    list_display = ['id', 'user', 'product', 'quantity']
